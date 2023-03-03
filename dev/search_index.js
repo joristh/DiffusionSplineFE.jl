bec934cc18d19ab522ec96366c88a1d41c474576
Documenter.jl <documenter@juliadocs.github.io>
2023-03-03 11:54:01 +0000

    build based on 36890a2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DiffusionSplineFE","category":"page"},{"location":"#DiffusionSplineFE","page":"Home","title":"DiffusionSplineFE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DiffusionSplineFE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DiffusionSplineFE]","category":"page"}]
+}
